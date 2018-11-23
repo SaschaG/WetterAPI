d5a09edffa698fa36526d9f8f9b96bf7a32497b8
Sascha Gundel <saschagundel@Saschas-MBP.fritz.box>
2018-11-23 02:07:57 +0100

    Added the api module and built method getForecastByName, including 2 tests. Moved testSpec in the correct folder.

--- program.js
+++ program.js
@@ -1,12 +1,14 @@
 const readline = require("readline");
+const a = require("./ApiConnector.js");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-rl.question("Please enter a city name or ZIP-Code, for detailed weather information.", (answer) => {
+rl.question("Please enter a city name, for detailed weather information.", (answer) => {
   //use the answer further for the api
 
   rl.close();
 });
+
