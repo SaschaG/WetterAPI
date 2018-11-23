f07f832d6d1557d9dbb93c14ebeb23f3391a1cb0
Sascha Gundel <saschagundel@Saschas-MBP.fritz.box>
2018-11-23 02:23:12 +0100

    Using the APIConnector now in program.js. Even though I hadn't enough time for formating and filtering the relevant informations

--- program.js
+++ program.js
@@ -1,13 +1,23 @@
 const readline = require("readline");
-const a = require("./ApiConnector.js");
-
+const ApiConnector = require("./ApiConnector.js");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-rl.question("Please enter a city name of germany, for detailed weather information.", (answer) => {
+
+rl.question("Please enter a city name of germany, for detailed weather information:\n", (answer) => {
   
+
+	ApiConnector.getForecastByName(answer)
+    .then(function(result) {
+
+    	console.log(JSON.stringify(result));
+	}, function(err) {
+
+		console.log(err);
+	});
+
 
   rl.close();
 });
